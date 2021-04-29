0809e1f419602e2f733e40ccd4da030d7dc941f9
Bernardo Piuma <bpiuma.uy@gmail.com>
2021-04-29 18:16:33 +0000

    Simple Counter v1.0

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,43 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Icon, InlineIcon } from "@iconify/react";
+import clockIcon from "@iconify-icons/il/clock";
 
 //create your first component
-export function Home() {
+export function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container d-flex justify-content-center align-items-center">
+			<div className="digito col-2 bg-dark m-3 pt-4 d-flex justify-content-center align-items-center">
+				<Icon icon={clockIcon} color="white" />
+			</div>
+			<div className="digito col-1 bg-dark m-3 d-flex justify-content-center align-items-center">
+				{props.dig6}
+			</div>
+			<div className="digito col-1 bg-dark m-3 d-flex justify-content-center align-items-center">
+				{props.dig5}
+			</div>
+			<div className="digito col-1 bg-dark m-3 d-flex justify-content-center align-items-center">
+				{props.dig4}
+			</div>
+			<div className="digito col-1 bg-dark m-3 d-flex justify-content-center align-items-center">
+				{props.dig3}
+			</div>
+			<div className="digito col-1 bg-dark m-3 d-flex justify-content-center align-items-center">
+				{props.dig2}
+			</div>
+			<div className="digito col-1 bg-dark m-3 d-flex justify-content-center align-items-center">
+				{props.dig1}
+			</div>
 		</div>
 	);
 }
+
+Home.propTypes = {
+	dig1: PropTypes.number,
+	dig2: PropTypes.number,
+	dig3: PropTypes.number,
+	dig4: PropTypes.number,
+	dig5: PropTypes.number,
+	dig6: PropTypes.number
+};
--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,17 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(() => {
+	let d1 = Math.floor(counter / 1) % 10;
+	let d2 = Math.floor(counter / 10) % 10;
+	let d3 = Math.floor(counter / 100) % 10;
+	let d4 = Math.floor(counter / 1000) % 10;
+	let d5 = Math.floor(counter / 10000) % 10;
+	let d6 = Math.floor(counter / 100000) % 10;
+	ReactDOM.render(
+		<Home dig1={d1} dig2={d2} dig3={d3} dig4={d4} dig5={d5} dig6={d6} />,
+		document.querySelector("#app")
+	);
+	counter++;
+}, 0.0001);
